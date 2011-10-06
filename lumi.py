0c3f3117944fd01b78cb133ceb75990e4e57665f
Paul Papacz <papacz@physik.rwth-aachen.de>
2011-10-06 20:38:19 +0200

    lumi.py: Convert the unit to pb^-1 always.
    lumiCalc2.py gives the luminosity in different units. Now it is always converted
    to pb-^1.
    It is also possible to choose the lumiCalc script and to switch on and off the
    corrections.

--- lumi.py
+++ lumi.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+import datetime
 import sys
 import os
 import optparse
+import re
 from ConfigParser import SafeConfigParser
 import subprocess
 from fnmatch import fnmatch
@@ -12,18 +14,33 @@ from FWCore.PythonUtilities.LumiList import LumiList
 lumi_dir = os.path.join( os.environ[ 'CMSSW_BASE' ], 'src/MUSiCProject/Skimming/test/lumi' )
 lumi_map_file = os.path.join( lumi_dir, 'lumi-map.txt' )
 
+date = datetime.datetime.today().isoformat('\0')
+
 parser = optparse.OptionParser( description='Calculate the integrated luminosity of CRAB tasks after applying additional luminosity-masks',  usage='usage: %prog [options] CRAB_TASKS...' )
 parser.add_option( '-d', '--lumi-dir', metavar='DIR', default=lumi_dir, help='Directory containing luminosity-masks [default: $CMSSW_BASE/src/MUSiCProject/Skimming/test/lumi]' )
 parser.add_option( '-l', '--lumi-map', metavar='FILE', default=lumi_map_file, help='Pattern file to map tasks on lumi-masks [default: $CMSSW_BASE/src/MUSiCProject/Skimming/test/lumi/lumi-map.txt]' )
 parser.add_option( '-a', '--all-lumi', action='store_true', default=False, help='Do not apply further lumi-masks [default: %default]' )
+parser.add_option( '-L', '--lumiCalc', action='store_true', default=False, help='Use lumiCalc.py instead of lumiCalc2.py [default: %default]' )
+parser.add_option( '-o', '--output', metavar='OUTFILE', help='Store output in OUTFILE [default: lumi-<date>.txt]' )
+parser.add_option( '-w', '--with-out', action='store_true', default=False, help='Do not use correction in lumi calculation [default: %default]' )
 
 (options, tasks ) = parser.parse_args()
+
+# default unit is /pb
+units = dict( [ ['/\xce\xbcb', 1e-6 ], [ '/ub', 1e-6 ], [ '/nb', 1e-3 ], [ '/pb', 1 ], [ '/fb', 1e3 ] ] ) # \xce\xbc represents the 'micro sign' in unicode
 
 if not tasks:
    parser.error( 'Needs at least one task to work on.' )
 
 del parser
 
+if options.lumiCalc:
+   options.lumi = 'lumiCalc.py'
+else:
+   options.lumi = 'lumiCalc2.py'
+
+if not options.output:
+   options.output = 'lumi-' + date + '.txt'
 
 #generate the lumi-mask map
 lumi_map = []
@@ -68,16 +85,25 @@ for task in tasks:
    jsons_to_read.append( output_file_name )
    print 'Task %s with dataset %s written to %s' % (task, datasetpath, output_file_name)
 
+# use tee to redirect stdout to a file
+tee = subprocess.Popen( [ 'tee', options.output ], stdin = subprocess.PIPE )
+os.dup2( tee.stdin.fileno(), sys.stdout.fileno() )
 
 print '\nCalculating integrated lumi in pb^-1:\n'
+
 #loop over generated lumi-files and get the lumi
 for json in jsons_to_read:
    print os.path.splitext( json )[0], '\t',
+
    #get the lumi information
-   proc = subprocess.Popen( [ 'lumiCalc2.py', '--with-correction', '-b', 'stable', 'overview', '-i', json ], stdout=subprocess.PIPE, stderr=subprocess.STDOUT )
+   args = [ options.lumi, '-b', 'stable', 'overview', '-i', json ]
+   if not options.with_out:
+     args += [ '--with-correction' ]
+   proc = subprocess.Popen( args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT )
+
    output = proc.communicate()[0]
    if proc.returncode != 0:
-      print '\nCalling lumiCalc.py failed. Output:'
+      print '\nCalling %s failed. Output:' %options.lumi
       print output
       sys.exit(1)
    #parse the output
@@ -88,13 +114,25 @@ for json in jsons_to_read:
       if 'Total' in i:
          break
    else:
-      print '\nUnexpected output from lumiCalc.py:'
+      print '\nUnexpected output from %s:' %options.lumi
       print output
       sys.exit(1)
-   #drop two lines
-   line.next()
+
+   # get unit
+   unit_line = line.next().split( '|' )
+   recorded = unit_line[4]
+   unit = re.compile('\(([^)]*)\)').search( recorded )
+   if unit:
+      unit = unit.groups()[0]
+   else:
+      print '\nUnexpected output from %s:' %options.lumi
+      print output
+      sys.exit(1)
+   conversion = units[ unit ]
+   #drop one line
    line.next()
    #now get the right line
    split_line = line.next().split( '|' )
    rec_lumi = float( split_line[4] )
+   rec_lumi *= conversion
    print rec_lumi
